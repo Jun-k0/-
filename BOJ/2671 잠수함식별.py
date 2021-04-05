bb32a8bd5931d49a0d81cede0d3e32a055bdd3a9
Jun-k0 <74391185+Jun-k0@users.noreply.github.com>
2021-04-05 17:49:31 +0900

    2671 잠수함식별.py : 정규표현식

---
+++ BOJ/2671 잠수함식별.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+input=sys.stdin.readline
+
+data=input().rstrip()
+p=re.compile('(100+1+|01)+')
+m=p.fullmatch(data)
+if m:
+    print("SUBMARINE")
+else:
+    print("NOISE")
+    
+
+'''
+파이썬 정규표현식 모듈 re
+https://wikidocs.net/4308 참고
+'''
