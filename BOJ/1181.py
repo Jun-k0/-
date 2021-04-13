58271aee3e68f459e1312cfed2a624367f1a0286
Jun-k0 <74391185+Jun-k0@users.noreply.github.com>
2021-04-13 23:59:32 +0900

    S5 정렬

---
+++ BOJ/1181.py
@@ -0,0 +1,15 @@
+import sys
+
+input=sys.stdin.readline
+
+n=int(input().rstrip())
+arr=[]
+for _ in range(n):
+    st=input().rstrip()
+    if st not in arr:
+        arr.append(st)
+arr.sort(key=lambda x:(len(x),x))
+
+for i in arr:
+    print(i)
+
