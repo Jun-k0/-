306f4a658d15796d49d3ace1c82e52af882a4cac
Jun-k0 <74391185+Jun-k0@users.noreply.github.com>
2021-04-28 19:03:26 +0900

    level2 이진법

---
+++ 프로그래머스/124 나라의 숫자.py
@@ -0,0 +1,24 @@
+def solution(n):
+    answer = ""
+    arr=[1,2,4]
+    cnt=1
+    val=4
+    while True:
+        if n<val:
+            val-=3**cnt
+            temp=n-val
+            while cnt>0:
+                cnt-=1
+                if temp//(3**cnt)>=1: 
+                    answer+=str(arr[temp//(3**cnt)]) # str 합치기
+                    temp%=3**cnt
+                else:
+                    answer+="1"
+            break
+        else:
+            cnt+=1
+            val+=3**cnt
+    return answer
+  '''
+  1 2 3 삼진법 풀이로 
+  '''
