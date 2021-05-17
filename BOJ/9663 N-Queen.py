f88c9e85f40dab4b95f53b771cc5e08a6418bc4d
Jun-k0 <74391185+Jun-k0@users.noreply.github.com>
2021-05-17 17:24:31 +0900

    G5 백트래킹

---
+++ BOJ/9663 N-Queen.py
@@ -0,0 +1,40 @@
+import sys
+
+input=sys.stdin.readline
+
+n=int(input())
+arr=[[0]*(n) for _ in range(n)]
+ans=[]
+res=0
+
+def chk(col,cnt):
+    for j in range(len(ans)):
+        if col in ans or abs(j-cnt)==abs(col-ans[j]):
+            return False
+    return True
+            
+
+def nqueen(cnt):
+    global res
+    if cnt==n:
+        # print(ans)
+        res+=1
+    else:
+        for i in range(n):
+            if cnt==0:
+                ans.append(int(i))
+                nqueen(cnt+1)
+                ans.pop()
+            else:
+                if chk(i,cnt):
+                    ans.append(int(i))
+                    nqueen(cnt+1)
+                    ans.pop()
+
+nqueen(0)
+print(res)
+
+'''
+한 줄에 하나씩만 넣는 걸 가정하고, row와 col중 하나만 보관하여
+반복문을 많이 줄일 수 있었따.
+'''
