49d8a7149a4ff6257460d4707bc8076e4e5623ca
Jun-k0 <74391185+Jun-k0@users.noreply.github.com>
2022-01-21 11:14:00 +0900

    행렬 회전

--- 알고리즘/행렬 회전.py
+++ 알고리즘/행렬 회전.py
@@ -1,8 +1,9 @@
 # 행렬 회전하는 법을 자꾸 까먹어서,,,,, 기록기록 
 
+# 시계 방향으로 90도 회전
 def rotate_a_matrix_by_90_degree(matrix):
-    rows = len(matrix)
-    columns = len(matrix[0])
+    row = len(matrix)
+    column = len(matrix[0])
     result=[[0] * row for _ in range(column)]
     for i in range(row):
         for j in range(column):
